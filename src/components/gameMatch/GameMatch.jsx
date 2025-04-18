ae29bf569db29e07e2e5b679bc10e20d21195d25
amarsanaa1234 <amarsanaa065@gmail.com>
2025-04-19 02:13:19 +0800

    hi bye

--- src/components/gameMatch/GameMatch.jsx
+++ src/components/gameMatch/GameMatch.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {supabase} from "../../tools/SupabaseClient.jsx";
 import {showMessage} from "../../tools/Tools.jsx";
-import {getKeyValue, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow} from "@heroui/react";
+import {Chip, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, User} from "@heroui/react";
 
 export const GameMatch = () => {
   const [gameDtlList, setGameDtlList] = useState([]);
@@ -20,11 +20,11 @@ export const GameMatch = () => {
     },
     {
       key: "team_three_point",
-      label: "Нийт оноо",
+      label: "Нийт 3н оноо",
     },
     {
       key: "team_rebount",
-      label: "Нийт оноо",
+      label: "Нийт самбар",
     },
     {
       key: "team_steal",
@@ -44,9 +44,76 @@ export const GameMatch = () => {
     },
   ];
 
+  const empColumns = [
+    {
+      key: "player_name",
+      label: "Нэр",
+    },
+    {
+      key: "point",
+      label: "PTS",
+    },
+    {
+      key: "three_point",
+      label: "3PTM",
+    },
+    {
+      key: "rebount",
+      label: "REB",
+    },
+    {
+      key: "steal",
+      label: "ST",
+    },
+    {
+      key: "block",
+      label: "BLK",
+    },
+    {
+      key: "turnovers",
+      label: "TO",
+    },
+    {
+      key: "total_free_throw",
+      label: "FTM/A*",
+    },
+    {
+      key: "fouls",
+      label: "Алдаа",
+    },
+  ];
+
   useEffect(() => {
     getGameDtl();
   }, [dtlId]);
+
+  const renderCell = (user, columnKey) => {
+    const cellValue = user[columnKey];
+
+    switch (columnKey) {
+      case "name":
+        return (
+            <User avatarProps={{radius: "lg", src: user.image}} description={user.email} name={cellValue}>
+              {user.name}
+            </User>
+        );
+      case "role":
+        return (
+            <div className="flex flex-col">
+              <p className="text-bold text-sm capitalize">{cellValue}</p>
+              <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+            </div>
+        );
+      case "win_or_loss":
+        return (
+            <Chip className="capitalize" color={cellValue == 1 ? 'success' : 'warning'} size="sm" variant="flat">
+              {cellValue == 3 ? '' :cellValue == 1 ? 'win' : 'loss'}
+            </Chip>
+        );
+      default:
+        return cellValue;
+    }
+  };
 
   const getGameDtl = async () => {
     const { data: gameDtl, error: gameDtlError } = await supabase
@@ -64,8 +131,8 @@ export const GameMatch = () => {
       sport_type,
       image
     ),
-    loss_team_id,
-    win_team_id,
+    master_win,
+    away_win,
     master_plus_minus,
     master_team_id,
     master_team_loss_point,
@@ -84,23 +151,94 @@ export const GameMatch = () => {
     if (gameDtlError) {
       showMessage({ type: "warning", text: "data дуудах үед алдаа гарлаа." });
     } else {
-      console.log(gameDtl);
-      setGameDtlList(gameDtl);
+
+      const data = [];
+      gameDtl.forEach((it, index) => {
+        const away_team_totals = {
+          key: `away-${index}`,
+          gameDtlid: it.id,
+          name: it.away_team.name,
+          sport_type: it.away_team.sport_type,
+          gender: it.away_team.gender,
+          image: it.away_team.image,
+          team_point: 0,
+          team_three_point: 0,
+          team_rebount: 0,
+          team_steal: 0,
+          team_free_throw: 0,
+          plus_minus: 0,
+          win_or_loss: it.away_win === it.master_win ? 3 : it.away_win
+        };
+
+        const master_team_totals = {
+          key: `master-${index}`,
+          gameDtlid: it.id,
+          name: it.master_team.name,
+          sport_type: it.master_team.sport_type,
+          gender: it.master_team.gender,
+          image: it.master_team.image,
+          team_point: 0,
+          team_three_point: 0,
+          team_rebount: 0,
+          team_steal: 0,
+          team_free_throw: 0,
+          plus_minus: 0,
+          win_or_loss: it.master_win === it.away_win ? 3 : it.master_win
+        };
+
+        data.push(away_team_totals);
+        data.push(master_team_totals);
+      });
+      setGameDtlList(data);
     }
   };
 
   return(
-    <Table aria-label="Example table with dynamic content">
-      <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={gameDtlList}>
-        {(item) => (
-          <TableRow key={item.key}>
-            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+    <div className={'w-full flex flex-col justify-center items-center gap-10'}>
+      <Table isStriped className='w-full' aria-label="Team statistics table">
+        <TableHeader columns={columns}>
+          {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+        </TableHeader>
+        <TableBody items={gameDtlList}>
+          {(item) => (
+              <TableRow key={item.id}>
+                {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+              </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <div className="w-full flex flex-row justify-center items-start gap-10 overflow-x-auto">
+        <div className="min-w-[500px]">
+          <Table isStriped className="w-full" aria-label="Team statistics table">
+            <TableHeader columns={empColumns}>
+              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={gameDtlList}>
+              {(item) => (
+                  <TableRow key={item.key}>
+                    {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                  </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+
+        <div className="min-w-[500px]">
+          <Table isStriped className="w-full" aria-label="Team statistics table">
+            <TableHeader columns={empColumns}>
+              {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={gameDtlList}>
+              {(item) => (
+                  <TableRow key={item.key}>
+                    {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                  </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+
+    </div>
   );
 }
